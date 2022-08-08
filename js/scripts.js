29b6650b33cb94a68feb30594ce91e91eddd6f78
ernas93 <107477572+ernas93@users.noreply.github.com>
2022-08-09 00:44:43 +0200

    wrapped in IIFE, added functions for getAll and add

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,44 @@
-let pokemonList = [
-    {
-        name: 'Chansey',
-        height: 1.1,
-        types: [
-            'NORMAL'
-        ]
-    },
-    {   
-        name: 'Pikachu',
-        height: 0.4,
-        types: [
-            'ELECTRIC'
-        ]
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Chansey',
+            height: 1.1,
+            types: [
+                'NORMAL'
+            ]
+        },
+        {   
+            name: 'Pikachu',
+            height: 0.4,
+            types: [
+                'ELECTRIC'
+            ]
 
-    },
-    {
-        name: 'Jigglypuff',
-        height: 0.5,
-        types: [
-            'FAIRY', 
-            'NORMAL'
-        ]
+        },
+        {
+            name: 'Jigglypuff',
+            height: 0.5,
+            types: [
+                'FAIRY', 
+                'NORMAL'
+            ]
 
+        }
+    ];
+
+    function getAll() {
+        return pokemonList;
     }
-];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
 
 // KEEPING THIS HERE FOR FUTURE REFERENCES
 // // this loop iterates over pokemonList and writes their names and height
@@ -36,6 +51,8 @@ let pokemonList = [
 //     }
 // }
 
+let pokemonList = pokemonRepository.getAll();
+
 pokemonList.forEach(function(pokemon) {
     if (pokemon.height>1) {
         document.write('<p>' + pokemon.name + ', height: ' + pokemon.height + ' - Wow, that\'s big!' + '</p>')
@@ -43,4 +60,4 @@ pokemonList.forEach(function(pokemon) {
         document.write('<p>' + pokemon.name + ', height: ' + pokemon.height + '</p>')
 
     }
-})
+});
